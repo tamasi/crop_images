20193132fb960e8c43730c2007da3ff642de03d6
hernan <hernan.bordon@gmail.com>
2017-12-15 14:40:46 -0300

    cambios en el script de ccorte

--- imagecrop.rb
+++ imagecrop.rb
@@ -12,16 +12,20 @@ def process_image(file, side, page_number)
   offset = (side == 'right')? image.width/2 : 0
   image = resize_and_crop(image, image.width, image.height, offset)
   #image.write "pages/#{file}_#{side}.tif"
-  image.write "pages/inhide01-01_#{page_number}.tif"
+  image.write "pages/inhide10-03_#{page_number.to_s.rjust(3,'0')}.tif"
 end
-page_number = 0
+page_number = 000
 Dir.foreach('images') do |file|
   next if ['.', '..', '.DS_Store'].include? file || File.directory?(file)
   puts file
   puts "*"*65
 
-  page_number += 1
-  process_image(file, 'left', page_number)
+  if page_number == 0
+    process_image(file, 'left', page_number)
+  else   
+    page_number += 1
+    process_image(file, 'left', page_number)
+  end
   page_number += 1
   process_image(file, 'right', page_number)
 end
